df6a5f404502c94904e70f9c69847e3a48c582c9
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-08-17 19:20:29 +0900

    👵

--- TechPod/AppDelegate.swift
+++ TechPod/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -29,6 +30,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptionsLaunchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playback, mode: .default)
+        } catch {
+            fatalError("カテゴリ設定失敗")
+        }
+        
+        do {
+            try session.setActive(true)
+        } catch {
+            fatalError("session有効化失敗")
+        }
+        
+        return true
+        
     }
 
 
